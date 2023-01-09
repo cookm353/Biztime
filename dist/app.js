feee409f71605afc693e8aeeab5ea33ec01105ca
Matthew Cook <cookm353@gmail.com>
2023-01-09 15:15:32 -0500

    reorganizing, importing routes

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const ExpressError = require("./expressError");
+const companyRouter = require("./routes/companies");
+const invoiceRouter = require("./routes/invoices");
 app.use(express.json());
 /** 404 handler */
 app.use(function (req, res, next) {
     const err = new ExpressError("Not Found", 404);
     return next(err);
 });
-/** general error handler */
+/** General error handler */
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     return res.json({
